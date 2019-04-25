8d88d368941ba45ff1a7d1646bb5bcbdfdff4317
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-04-25 14:51:40 -0400

    Add Go v1.12.3. (#2)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 1.12/Dockerfile -t circleciimages/golang:1.12.2  -t circleciimages/golang:1.12 .
+docker build --file 1.12/Dockerfile -t circleciimages/golang:1.12.3  -t circleciimages/golang:1.12 .
