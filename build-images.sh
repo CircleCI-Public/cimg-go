9e96490aa19c0f60b1aeffb99a8b4ed63bc35ac3
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-07-14 13:49:05 -0400

    Publish v1.13.13 and v1.14.5. [release] (#84)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 1.13/Dockerfile -t cimg/go:1.13.12  -t cimg/go:1.13 .
-docker build --file 1.13/node/Dockerfile -t cimg/go:1.13.12-node  -t cimg/go:1.13-node .
-docker build --file 1.14/Dockerfile -t cimg/go:1.14.4  -t cimg/go:1.14 .
-docker build --file 1.14/node/Dockerfile -t cimg/go:1.14.4-node  -t cimg/go:1.14-node .
+docker build --file 1.13/Dockerfile -t cimg/go:1.13.13  -t cimg/go:1.13 .
+docker build --file 1.13/node/Dockerfile -t cimg/go:1.13.13-node  -t cimg/go:1.13-node .
+docker build --file 1.14/Dockerfile -t cimg/go:1.14.5  -t cimg/go:1.14 .
+docker build --file 1.14/node/Dockerfile -t cimg/go:1.14.5-node  -t cimg/go:1.14-node .
