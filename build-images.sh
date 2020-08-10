1e49f8d53fce4728d7ceea7c846a49c0272e0261
Carlos Panato <ctadeu@gmail.com>
2020-08-10 16:07:59 +0200

    add 1.15 in the build script

--- build-images.sh
+++ build-images.sh
@@ -4,3 +4,5 @@ docker build --file 1.13/Dockerfile -t cimg/go:1.13.15  -t cimg/go:1.13 .
 docker build --file 1.13/node/Dockerfile -t cimg/go:1.13.15-node  -t cimg/go:1.13-node .
 docker build --file 1.14/Dockerfile -t cimg/go:1.14.7  -t cimg/go:1.14 .
 docker build --file 1.14/node/Dockerfile -t cimg/go:1.14.7-node  -t cimg/go:1.14-node .
+docker build --file 1.15/Dockerfile -t cimg/go:1.15rc2  -t cimg/go:1.15  .
+docker build --file 1.15/node/Dockerfile -t cimg/go:1.15rc2-node  -t cimg/go:1.15-node .
