68ff98067a9d07bf96fa44666ad1f0898e7dc4f8
Jawad Seddar <jawad.seddar@algolia.com>
2020-09-01 19:24:03 +0200

    Publish v1.14.8 and v1.15.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 1.15/Dockerfile -t cimg/go:1.15 .
-docker build --file 1.15/node/Dockerfile -t cimg/go:1.15-node .
+docker build --file 1.14/Dockerfile -t cimg/go:1.14.8  -t cimg/go:1.14 .
+docker build --file 1.14/node/Dockerfile -t cimg/go:1.14.8-node  -t cimg/go:1.14-node .
+docker build --file 1.15/Dockerfile -t cimg/go:1.15.1 -t cimg/go:1.15 .
+docker build --file 1.15/node/Dockerfile -t cimg/go:1.15.1-node -t cimg/go:1.15-node .
