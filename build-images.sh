144de8fce704bedd49bdec8478c7e099cbead5f2
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-13 17:47:17 -0500

    Respin browsers tags for xvfb, part 4. (#109)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 1.14/browsers/Dockerfile -t cimg/go:1.14.11-browsers  -t cimg/go:1.14-browsers .
-docker build --file 1.15/browsers/Dockerfile -t cimg/go:1.15.4-browsers  -t cimg/go:1.15-browsers .
+docker build --file 1.14/browsers/Dockerfile -t cimg/go:1.14.12-browsers  -t cimg/go:1.14-browsers .
+docker build --file 1.15/browsers/Dockerfile -t cimg/go:1.15.5-browsers  -t cimg/go:1.15-browsers .
