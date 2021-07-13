2734f29859564d9a295c158038ff96f83a1c5a74
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-07-13 14:49:14 -0400

    Publish v1.15.14 and v1.16.6. [release] (#131)

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 1.15/Dockerfile -t cimg/go:1.15.13  -t cimg/go:1.15 .
-docker build --file 1.15/node/Dockerfile -t cimg/go:1.15.13-node  -t cimg/go:1.15-node .
-docker build --file 1.15/browsers/Dockerfile -t cimg/go:1.15.13-browsers  -t cimg/go:1.15-browsers .
-docker build --file 1.16/Dockerfile -t cimg/go:1.16.5  -t cimg/go:1.16 .
-docker build --file 1.16/node/Dockerfile -t cimg/go:1.16.5-node  -t cimg/go:1.16-node .
-docker build --file 1.16/browsers/Dockerfile -t cimg/go:1.16.5-browsers  -t cimg/go:1.16-browsers .
+docker build --file 1.15/Dockerfile -t cimg/go:1.15.14  -t cimg/go:1.15 .
+docker build --file 1.15/node/Dockerfile -t cimg/go:1.15.14-node  -t cimg/go:1.15-node .
+docker build --file 1.15/browsers/Dockerfile -t cimg/go:1.15.14-browsers  -t cimg/go:1.15-browsers .
+docker build --file 1.16/Dockerfile -t cimg/go:1.16.6  -t cimg/go:1.16 .
+docker build --file 1.16/node/Dockerfile -t cimg/go:1.16.6-node  -t cimg/go:1.16-node .
+docker build --file 1.16/browsers/Dockerfile -t cimg/go:1.16.6-browsers  -t cimg/go:1.16-browsers .
