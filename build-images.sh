74196a44b5b3a2f2a99040b54afd9b1cb2da1a51
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-09-10 01:20:04 -0400

    Publish v1.17.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.17/Dockerfile -t cimg/go:1.17 .
-docker build --file 1.17/node/Dockerfile -t cimg/go:1.17-node .
-docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17-browsers .
+docker build --file 1.17/Dockerfile -t cimg/go:1.17.1  -t cimg/go:1.17 .
+docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.1-node  -t cimg/go:1.17-node .
+docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.1-browsers  -t cimg/go:1.17-browsers .
