34d88af54ab5166cc9906312bc30c59be2562cde
Adam Hartley <BytesGuy@users.noreply.github.com>
2021-12-09 14:05:05 +0000

    Publish v1.16.12 and v1.17.5. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 1.16/Dockerfile -t cimg/go:1.16.11  -t cimg/go:1.16 .
-docker build --file 1.16/node/Dockerfile -t cimg/go:1.16.11-node  -t cimg/go:1.16-node .
-docker build --file 1.16/browsers/Dockerfile -t cimg/go:1.16.11-browsers  -t cimg/go:1.16-browsers .
-docker build --file 1.17/Dockerfile -t cimg/go:1.17.4  -t cimg/go:1.17 .
-docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.4-node  -t cimg/go:1.17-node .
-docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.4-browsers  -t cimg/go:1.17-browsers .
+docker build --file 1.16/Dockerfile -t cimg/go:1.16.12  -t cimg/go:1.16 .
+docker build --file 1.16/node/Dockerfile -t cimg/go:1.16.12-node  -t cimg/go:1.16-node .
+docker build --file 1.16/browsers/Dockerfile -t cimg/go:1.16.12-browsers  -t cimg/go:1.16-browsers .
+docker build --file 1.17/Dockerfile -t cimg/go:1.17.5  -t cimg/go:1.17 .
+docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.5-node  -t cimg/go:1.17-node .
+docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.5-browsers  -t cimg/go:1.17-browsers .
