f588b45d8bddb0ecf8b3b18dc138aa84402e4b7c
Ewout Prangsma <ewout@prangsma.net>
2021-12-19 19:07:50 +0100

    Fixed base image

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-DOCKERIMAGE=arangodboasis/cimg-go:1.17.5-${CIRCLE_TAG:-$CIRCLE_BRANCH-$CIRCLE_SHA1}
+GOVERSION=1.17.5
+DOCKERIMAGE=arangodboasis/cimg-go:${GOVERSION}-${CIRCLE_TAG:-$CIRCLE_BRANCH-$CIRCLE_SHA1}
 echo Building ${DOCKERIMAGE}
 
-docker buildx install
-docker build \
-    --build-arg CIMGBASETAG=${CIMGBASETAG} \
-    --platform linux/amd64,linux/arm64 \
-    --push \
-    --file 1.17/Dockerfile \
-    -t ${DOCKERIMAGE} .
+if [ "$1" = "latest" ]; then
+    ~/regctl image copy ${DOCKERIMAGE} arangodboasis/cimg-go:${GOVERSION}-latest
+else
+    docker buildx install
+    docker build \
+        --build-arg CIMGBASETAG=${CIMGBASETAG} \
+        --platform linux/amd64,linux/arm64 \
+        --push \
+        --file 1.17/Dockerfile \
+        -t ${DOCKERIMAGE} .
+fi
