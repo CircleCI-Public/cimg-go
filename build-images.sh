62822a25898cc42e9ba0ea76365cae5d02de5616
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-03-04 03:54:15 +0000

    Publish v1.16.15. [release] (#156)
    Co-authored-by: Ricardo N Feliciano <FelicianoTech@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.17/Dockerfile -t cimg/go:1.17.8  -t cimg/go:1.17 .
-docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.8-node  -t cimg/go:1.17-node .
-docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.8-browsers  -t cimg/go:1.17-browsers .
+docker build --file 1.16/Dockerfile -t cimg/go:1.16.15  -t cimg/go:1.16 .
+docker build --file 1.16/node/Dockerfile -t cimg/go:1.16.15-node  -t cimg/go:1.16-node .
+docker build --file 1.16/browsers/Dockerfile -t cimg/go:1.16.15-browsers  -t cimg/go:1.16-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/go:1.17.8
-docker push cimg/go:1.17
-docker push cimg/go:1.17.8-node
-docker push cimg/go:1.17-node
-docker push cimg/go:1.17.8-browsers
-docker push cimg/go:1.17-browsers
+docker push cimg/go:1.16.15
+docker push cimg/go:1.16
+docker push cimg/go:1.16.15-node
+docker push cimg/go:1.16-node
+docker push cimg/go:1.16.15-browsers
+docker push cimg/go:1.16-browsers
