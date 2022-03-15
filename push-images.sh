e5c66425e7b9fd10193d5e107cd7481c6f844185
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-03-15 17:19:31 +0000

    Publish v1.18. [release] (#159)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/go:1.16.15
-docker push cimg/go:1.16
-docker push cimg/go:1.16.15-node
-docker push cimg/go:1.16-node
-docker push cimg/go:1.16.15-browsers
-docker push cimg/go:1.16-browsers
+docker push cimg/go:1.18
+docker push cimg/go:1.18-node
+docker push cimg/go:1.18-browsers
