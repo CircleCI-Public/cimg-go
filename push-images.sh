e8cc12aac71c533e5792e7a184a1315c23266bfa
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-03-30 19:24:29 -0400

    Publish v1.10.8. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/go:1.18.0
-docker push cimg/go:1.18
-docker push cimg/go:1.18.0-node
-docker push cimg/go:1.18-node
-docker push cimg/go:1.18.0-browsers
-docker push cimg/go:1.18-browsers
+docker push cimg/go:1.10.8
+docker push cimg/go:1.10
+docker push cimg/go:1.10.8-node
+docker push cimg/go:1.10-node
+docker push cimg/go:1.10.8-browsers
+docker push cimg/go:1.10-browsers
