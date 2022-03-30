d88c55b5739bcf961cacd006633173f0d5351c9e
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2022-03-30 18:46:25 -0400

    Respin Go v1.18 to fix two bugs [release] (#164)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.18/Dockerfile -t cimg/go:1.18 .
-docker build --file 1.18/node/Dockerfile -t cimg/go:1.18-node .
-docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18-browsers .
+docker build --file 1.18/Dockerfile -t cimg/go:1.18.0  -t cimg/go:1.18 .
+docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.0-node  -t cimg/go:1.18-node .
+docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.0-browsers  -t cimg/go:1.18-browsers .
