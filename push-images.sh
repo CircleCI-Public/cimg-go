b697c3ef95936dff92af751931c223951e2d534c
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2022-03-30 22:29:09 +0000

    Respin Go v1.18 to fix two bugs [release]

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
+docker push cimg/go:1.18.0
 docker push cimg/go:1.18
+docker push cimg/go:1.18.0-node
 docker push cimg/go:1.18-node
+docker push cimg/go:1.18.0-browsers
 docker push cimg/go:1.18-browsers
