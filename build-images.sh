4c514e247971252a1ae9c79c2da1b34af997ca05
Robert Stam <git@stamware.com>
2022-05-02 12:52:29 +0200

    Added fix for QEMU install

--- build-images.sh
+++ build-images.sh
@@ -8,6 +8,7 @@ if [ "$1" = "latest" ]; then
     ~/regctl image copy ${DOCKERIMAGE} arangodboasis/cimg-go:${GOVERSION}-latest
 else
     docker buildx install
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes --credential yes
     docker build \
         --build-arg CIMGBASETAG=${CIMGBASETAG} \
         --build-arg GO_VERSION=${GOVERSION} \
