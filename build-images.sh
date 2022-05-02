bf4ec553eb137346055335445cf5f45f573b211c
Robert Stam <git@stamware.com>
2022-05-02 13:11:22 +0200

    Switched to Ubuntu 18/04 as build image.

--- build-images.sh
+++ build-images.sh
@@ -8,7 +8,6 @@ if [ "$1" = "latest" ]; then
     ~/regctl image copy ${DOCKERIMAGE} arangodboasis/cimg-go:${GOVERSION}-latest
 else
     docker buildx install
-    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes --credential yes
     docker build \
         --build-arg CIMGBASETAG=${CIMGBASETAG} \
         --build-arg GO_VERSION=${GOVERSION} \
