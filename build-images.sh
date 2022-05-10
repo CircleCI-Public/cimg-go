40494e0a81157cbbc850762b9a2aa56c44f75399
Jeff Chen <jeff.chen@circleci.com>
2022-05-10 14:10:40 -0700

    Publish v1.17.10 and v1.18.2. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.17/Dockerfile -t cimg/go:1.17.9  -t cimg/go:1.17 .
-docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.9-node  -t cimg/go:1.17-node .
-docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.9-browsers  -t cimg/go:1.17-browsers .
-docker build --file 1.18/Dockerfile -t cimg/go:1.18.1  -t cimg/go:1.18 .
-docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.1-node  -t cimg/go:1.18-node .
-docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.1-browsers  -t cimg/go:1.18-browsers .
+docker build --file 1.17/Dockerfile -t cimg/go:1.17.10 -t cimg/go:1.17 .
+docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.10-node -t cimg/go:1.17-node .
+docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.10-browsers -t cimg/go:1.17-browsers .
+docker build --file 1.18/Dockerfile -t cimg/go:1.18.2 -t cimg/go:1.18 .
+docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.2-node -t cimg/go:1.18-node .
+docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.2-browsers -t cimg/go:1.18-browsers .
