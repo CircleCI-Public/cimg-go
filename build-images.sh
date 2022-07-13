5b75e3d6b8d2b1be3a0d7e17026e02bb73171b53
Jeff Chen <jeff.chen@circleci.com>
2022-07-13 16:09:27 -0700

    Publish v1.17.12 and v1.18.4. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.17/Dockerfile -t cimg/go:1.17.11 -t cimg/go:1.17 .
-docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.11-node -t cimg/go:1.17-node .
-docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.11-browsers -t cimg/go:1.17-browsers .
+docker build --file 1.17/Dockerfile -t cimg/go:1.17.12 -t cimg/go:1.17 .
+docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.12-node -t cimg/go:1.17-node .
+docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.12-browsers -t cimg/go:1.17-browsers .
+docker build --file 1.18/Dockerfile -t cimg/go:1.18.4 -t cimg/go:1.18 .
+docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.4-node -t cimg/go:1.18-node .
+docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.4-browsers -t cimg/go:1.18-browsers .
