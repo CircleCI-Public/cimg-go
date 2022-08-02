adec05bf1d6563758c55e5a9509371073ac9108c
Jalexchen <jeff.chen@circleci.com>
2022-08-02 16:25:14 -0700

    Publish v1.19.0 [release] (#188)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.17/Dockerfile -t cimg/go:1.17.13 -t cimg/go:1.17 .
-docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.13-node -t cimg/go:1.17-node .
-docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.13-browsers -t cimg/go:1.17-browsers .
-docker build --file 1.18/Dockerfile -t cimg/go:1.18.5 -t cimg/go:1.18 .
-docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.5-node -t cimg/go:1.18-node .
-docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.5-browsers -t cimg/go:1.18-browsers .
+docker build --file 1.19/Dockerfile -t cimg/go:1.19.0 -t cimg/go:1.19 .
+docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.0-node -t cimg/go:1.19-node .
+docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.0-browsers -t cimg/go:1.19-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/go:1.17
-docker push cimg/go:1.17.13
-docker push cimg/go:1.17-node
-docker push cimg/go:1.17.13-node
-docker push cimg/go:1.17-browsers
-docker push cimg/go:1.17.13-browsers
-docker push cimg/go:1.18
-docker push cimg/go:1.18.5
-docker push cimg/go:1.18-node
-docker push cimg/go:1.18.5-node
-docker push cimg/go:1.18-browsers
-docker push cimg/go:1.18.5-browsers
+docker push cimg/go:1.19
+docker push cimg/go:1.19.0
+docker push cimg/go:1.19-node
+docker push cimg/go:1.19.0-node
+docker push cimg/go:1.19-browsers
+docker push cimg/go:1.19.0-browsers
