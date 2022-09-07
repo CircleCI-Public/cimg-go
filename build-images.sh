48448c584a346740b4c4035292e06be1c14e04da
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-09-07 15:14:57 -0400

    Publish v1.18.6 and v1.19.1. [release] (#194)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.19/Dockerfile -t cimg/go:1.19.0 -t cimg/go:1.19 .
-docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.0-node -t cimg/go:1.19-node .
-docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.0-browsers -t cimg/go:1.19-browsers .
+docker build --file 1.18/Dockerfile -t cimg/go:1.18.6 -t cimg/go:1.18 .
+docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.6-node -t cimg/go:1.18-node .
+docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.6-browsers -t cimg/go:1.18-browsers .
+docker build --file 1.19/Dockerfile -t cimg/go:1.19.1 -t cimg/go:1.19 .
+docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.1-node -t cimg/go:1.19-node .
+docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.1-browsers -t cimg/go:1.19-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/go:1.18
+docker push cimg/go:1.18.6
+docker push cimg/go:1.18-node
+docker push cimg/go:1.18.6-node
+docker push cimg/go:1.18-browsers
+docker push cimg/go:1.18.6-browsers
 docker push cimg/go:1.19
-docker push cimg/go:1.19.0
+docker push cimg/go:1.19.1
 docker push cimg/go:1.19-node
-docker push cimg/go:1.19.0-node
+docker push cimg/go:1.19.1-node
 docker push cimg/go:1.19-browsers
-docker push cimg/go:1.19.0-browsers
+docker push cimg/go:1.19.1-browsers
