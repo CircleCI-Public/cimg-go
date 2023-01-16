498dc061146ff99508a7bb22f814974a94caf31d
Adam Hartley <git@ahartley.com>
2023-01-16 22:14:19 +0000

    Publish v1.18.10 and v1.19.5. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.18/Dockerfile -t cimg/go:1.18.9 -t cimg/go:1.18 .
-docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.9-node -t cimg/go:1.18-node .
-docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.9-browsers -t cimg/go:1.18-browsers .
-docker build --file 1.19/Dockerfile -t cimg/go:1.19.4 -t cimg/go:1.19 .
-docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.4-node -t cimg/go:1.19-node .
-docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.4-browsers -t cimg/go:1.19-browsers .
+docker build --file 1.18/Dockerfile -t cimg/go:1.18.10 -t cimg/go:1.18 .
+docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.10-node -t cimg/go:1.18-node .
+docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.10-browsers -t cimg/go:1.18-browsers .
+docker build --file 1.19/Dockerfile -t cimg/go:1.19.5 -t cimg/go:1.19 .
+docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.5-node -t cimg/go:1.19-node .
+docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.5-browsers -t cimg/go:1.19-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/go:1.18
-docker push cimg/go:1.18.9
+docker push cimg/go:1.18.10
 docker push cimg/go:1.18-node
-docker push cimg/go:1.18.9-node
+docker push cimg/go:1.18.10-node
 docker push cimg/go:1.18-browsers
-docker push cimg/go:1.18.9-browsers
+docker push cimg/go:1.18.10-browsers
 docker push cimg/go:1.19
-docker push cimg/go:1.19.4
+docker push cimg/go:1.19.5
 docker push cimg/go:1.19-node
-docker push cimg/go:1.19.4-node
+docker push cimg/go:1.19.5-node
 docker push cimg/go:1.19-browsers
-docker push cimg/go:1.19.4-browsers
+docker push cimg/go:1.19.5-browsers
