51c5970d08d881e2fd5603a3263baf47639d8225
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-02-01 18:03:15 -0500

    Publish v1.20.0. [release] (#212)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.18/Dockerfile -t cimg/go:1.18.10 -t cimg/go:1.18 .
-docker build --file 1.18/node/Dockerfile -t cimg/go:1.18.10-node -t cimg/go:1.18-node .
-docker build --file 1.18/browsers/Dockerfile -t cimg/go:1.18.10-browsers -t cimg/go:1.18-browsers .
-docker build --file 1.19/Dockerfile -t cimg/go:1.19.5 -t cimg/go:1.19 .
-docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.5-node -t cimg/go:1.19-node .
-docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.5-browsers -t cimg/go:1.19-browsers .
+docker build --file 1.20/Dockerfile -t cimg/go:1.20.0 -t cimg/go:1.20 .
+docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.0-node -t cimg/go:1.20-node .
+docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.0-browsers -t cimg/go:1.20-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/go:1.18
-docker push cimg/go:1.18.10
-docker push cimg/go:1.18-node
-docker push cimg/go:1.18.10-node
-docker push cimg/go:1.18-browsers
-docker push cimg/go:1.18.10-browsers
-docker push cimg/go:1.19
-docker push cimg/go:1.19.5
-docker push cimg/go:1.19-node
-docker push cimg/go:1.19.5-node
-docker push cimg/go:1.19-browsers
-docker push cimg/go:1.19.5-browsers
+docker push cimg/go:1.20
+docker push cimg/go:1.20.0
+docker push cimg/go:1.20-node
+docker push cimg/go:1.20.0-node
+docker push cimg/go:1.20-browsers
+docker push cimg/go:1.20.0-browsers
