8d8547516cfecc6c6f565f979bf9fc393eb0168d
jalexbot <jeff.chen@circleci.com>
2023-02-15 01:58:09 +0000

    Publish v1.20.1 and v1.19.6. [release]

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,7 @@
 # Do not edit by hand; please use build scripts/templates to make changes
 set -eo pipefail
 
+<<<<<<< HEAD
 docker context create cimg
 docker buildx create --use cimg
 docker buildx build --platform=linux/amd64,linux/arm64 --file 1.21/Dockerfile -t cimg/go:1.21.1 -t cimg/go:1.21 --push .
@@ -13,3 +14,11 @@ docker buildx build --platform=linux/amd64 --file 1.20/browsers/Dockerfile -t ci
 docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/Dockerfile -t cimg/go:1.19.13 -t cimg/go:1.19 --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/node/Dockerfile -t cimg/go:1.19.13-node -t cimg/go:1.19-node --push .
 docker buildx build --platform=linux/amd64 --file 1.19/browsers/Dockerfile -t cimg/go:1.19.13-browsers -t cimg/go:1.19-browsers --push .
+=======
+docker build --file 1.20/Dockerfile -t cimg/go:1.20.1 -t cimg/go:1.20 .
+docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.1-node -t cimg/go:1.20-node .
+docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.1-browsers -t cimg/go:1.20-browsers .
+docker build --file 1.19/Dockerfile -t cimg/go:1.19.6 -t cimg/go:1.19 .
+docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.6-node -t cimg/go:1.19-node .
+docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.6-browsers -t cimg/go:1.19-browsers .
+>>>>>>> 6a878e9 (Publish v1.20.1 and v1.19.6. [release])
