1da33ab458dc80c888d8e10153b86d69cde5a98e
mkusaka <hinoshita1992@gmail.com>
2023-02-15 23:30:22 +0900

    Publish v1.20.1 and v1.19.6. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.20/Dockerfile -t cimg/go:1.20.0 -t cimg/go:1.20 .
-docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.0-node -t cimg/go:1.20-node .
-docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.0-browsers -t cimg/go:1.20-browsers .
+docker build --file 1.20/Dockerfile -t cimg/go:1.20.1 -t cimg/go:1.20 --platform linux/amd64 .
+docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.1-node -t cimg/go:1.20-node --platform linux/amd64 .
+docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.1-browsers -t cimg/go:1.20-browsers --platform linux/amd64 .
+docker build --file 1.19/Dockerfile -t cimg/go:1.19.6 -t cimg/go:1.19 --platform linux/amd64 .
+docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.6-node -t cimg/go:1.19-node --platform linux/amd64 .
+docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.6-browsers -t cimg/go:1.19-browsers --platform linux/amd64 .
