4e797580a9fa83fb3e1edd17cb199d2ae967aa72
mkusaka <hinoshita1992@gmail.com>
2023-02-16 01:44:42 +0900

    Publish v1.20.1 and v1.19.6. [release] (#217)

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/go:1.20
-docker push cimg/go:1.20.0
+docker push cimg/go:1.20.1
 docker push cimg/go:1.20-node
-docker push cimg/go:1.20.0-node
+docker push cimg/go:1.20.1-node
 docker push cimg/go:1.20-browsers
-docker push cimg/go:1.20.0-browsers
+docker push cimg/go:1.20.1-browsers
+docker push cimg/go:1.19
+docker push cimg/go:1.19.6
+docker push cimg/go:1.19-node
+docker push cimg/go:1.19.6-node
+docker push cimg/go:1.19-browsers
+docker push cimg/go:1.19.6-browsers
