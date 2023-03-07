efb4176d4c535e3f9d09a7ae8638a95ff22192e4
jalexbot <jeff.chen@circleci.com>
2023-03-07 18:27:51 +0000

    Publish v1.20.2 and v1.19.7. [release]

--- build-images.sh
+++ build-images.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 docker context create cimg
 docker buildx create --use cimg
 docker buildx build --platform=linux/amd64,linux/arm64 --file 1.21/Dockerfile -t cimg/go:1.21.1 -t cimg/go:1.21 --push .
@@ -22,3 +23,11 @@ docker build --file 1.19/Dockerfile -t cimg/go:1.19.6 -t cimg/go:1.19 .
 docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.6-node -t cimg/go:1.19-node .
 docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.6-browsers -t cimg/go:1.19-browsers .
 >>>>>>> 6a878e9 (Publish v1.20.1 and v1.19.6. [release])
+=======
+docker build --file 1.20/Dockerfile -t cimg/go:1.20.2 -t cimg/go:1.20 .
+docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.2-node -t cimg/go:1.20-node .
+docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.2-browsers -t cimg/go:1.20-browsers .
+docker build --file 1.19/Dockerfile -t cimg/go:1.19.7 -t cimg/go:1.19 .
+docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.7-node -t cimg/go:1.19-node .
+docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.7-browsers -t cimg/go:1.19-browsers .
+>>>>>>> 973b590 (Publish v1.20.2 and v1.19.7. [release])
--- push-images.sh
+++ push-images.sh
@@ -4,15 +4,19 @@
 set -eo pipefail
 =======
 docker push cimg/go:1.20
-docker push cimg/go:1.20.1
+docker push cimg/go:1.20.2
 docker push cimg/go:1.20-node
-docker push cimg/go:1.20.1-node
+docker push cimg/go:1.20.2-node
 docker push cimg/go:1.20-browsers
-docker push cimg/go:1.20.1-browsers
+docker push cimg/go:1.20.2-browsers
 docker push cimg/go:1.19
-docker push cimg/go:1.19.6
+docker push cimg/go:1.19.7
 docker push cimg/go:1.19-node
-docker push cimg/go:1.19.6-node
+docker push cimg/go:1.19.7-node
 docker push cimg/go:1.19-browsers
+<<<<<<< HEAD
 docker push cimg/go:1.19.6-browsers
 >>>>>>> 6a878e9 (Publish v1.20.1 and v1.19.6. [release])
+=======
+docker push cimg/go:1.19.7-browsers
+>>>>>>> 973b590 (Publish v1.20.2 and v1.19.7. [release])
