92cfc1fa6615a6eea83933307773b04e193b842b
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-03-07 20:06:47 -0500

    Publish v1.19.7 and v1.20.2. [release] (#220)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.20/Dockerfile -t cimg/go:1.20.1 -t cimg/go:1.20 --platform linux/amd64 .
-docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.1-node -t cimg/go:1.20-node --platform linux/amd64 .
-docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.1-browsers -t cimg/go:1.20-browsers --platform linux/amd64 .
-docker build --file 1.19/Dockerfile -t cimg/go:1.19.6 -t cimg/go:1.19 --platform linux/amd64 .
-docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.6-node -t cimg/go:1.19-node --platform linux/amd64 .
-docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.6-browsers -t cimg/go:1.19-browsers --platform linux/amd64 .
+docker build --file 1.19/Dockerfile -t cimg/go:1.19.7 -t cimg/go:1.19 --platform linux/amd64 .
+docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.7-node -t cimg/go:1.19-node --platform linux/amd64 .
+docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.7-browsers -t cimg/go:1.19-browsers --platform linux/amd64 .
+docker build --file 1.20/Dockerfile -t cimg/go:1.20.2 -t cimg/go:1.20 --platform linux/amd64 .
+docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.2-node -t cimg/go:1.20-node --platform linux/amd64 .
+docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.2-browsers -t cimg/go:1.20-browsers --platform linux/amd64 .
