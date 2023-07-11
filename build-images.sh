efc9341d29c1789d1f344592f6f111f154c9fe24
Adam Hartley <git@ahartley.com>
2023-07-11 22:46:18 +0100

    Publish v1.20.6 and v1.19.10. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.20/Dockerfile -t cimg/go:1.20.5 -t cimg/go:1.20 --platform linux/amd64 .
-docker build --file 1.20/node/Dockerfile -t cimg/go:1.20.5-node -t cimg/go:1.20-node --platform linux/amd64 .
-docker build --file 1.20/browsers/Dockerfile -t cimg/go:1.20.5-browsers -t cimg/go:1.20-browsers --platform linux/amd64 .
-docker build --file 1.19/Dockerfile -t cimg/go:1.19.10 -t cimg/go:1.19 --platform linux/amd64 .
-docker build --file 1.19/node/Dockerfile -t cimg/go:1.19.10-node -t cimg/go:1.19-node --platform linux/amd64 .
-docker build --file 1.19/browsers/Dockerfile -t cimg/go:1.19.10-browsers -t cimg/go:1.19-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.20/Dockerfile -t cimg/go:1.20.6 -t cimg/go:1.20 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.20/node/Dockerfile -t cimg/go:1.20.6-node -t cimg/go:1.20-node --push .
+docker buildx build --platform=linux/amd64 --file 1.20/browsers/Dockerfile -t cimg/go:1.20.6-browsers -t cimg/go:1.20-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/Dockerfile -t cimg/go:1.19.10 -t cimg/go:1.19 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/node/Dockerfile -t cimg/go:1.19.10-node -t cimg/go:1.19-node --push .
+docker buildx build --platform=linux/amd64 --file 1.19/browsers/Dockerfile -t cimg/go:1.19.10-browsers -t cimg/go:1.19-browsers --push .
--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,2 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/go:1.20
-docker push cimg/go:1.20.5
-docker push cimg/go:1.20-node
-docker push cimg/go:1.20.5-node
-docker push cimg/go:1.20-browsers
-docker push cimg/go:1.20.5-browsers
-docker push cimg/go:1.19
-docker push cimg/go:1.19.10
-docker push cimg/go:1.19-node
-docker push cimg/go:1.19.10-node
-docker push cimg/go:1.19-browsers
-docker push cimg/go:1.19.10-browsers
