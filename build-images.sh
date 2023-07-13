f3c0a13d8b8d125525cb8484601bc2d7c45a210a
Adam Hartley <git@ahartley.com>
2023-07-13 11:42:25 +0100

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash -eo pipefail
+#!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
+set -eo pipefail
 docker context create cimg
 docker buildx create --use cimg
 docker buildx build --platform=linux/amd64,linux/arm64 --file 1.20/Dockerfile -t cimg/go:1.20.6 -t cimg/go:1.20 --push .
