69e4f20c6a02a5dc5421a69966becf4a5c232ca6
Adam Hartley <git@ahartley.com>
2023-07-13 10:40:52 +0100

    Publish v1.20.6 and v1.19.10. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/go:1.20
-docker push cimg/go:1.20.5
+docker push cimg/go:1.20.6
 docker push cimg/go:1.20-node
-docker push cimg/go:1.20.5-node
+docker push cimg/go:1.20.6-node
 docker push cimg/go:1.20-browsers
-docker push cimg/go:1.20.5-browsers
+docker push cimg/go:1.20.6-browsers
 docker push cimg/go:1.19
 docker push cimg/go:1.19.10
 docker push cimg/go:1.19-node
