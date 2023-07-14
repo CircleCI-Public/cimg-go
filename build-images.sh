0a44a70fd34b38867418cde9e3494858e8255ed1
Jeff Chen <jeff.chen@circleci.com>
2023-07-14 11:43:55 -0700

    test arm build

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
--- push-images.sh
+++ push-images.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-set -eo pipefail
