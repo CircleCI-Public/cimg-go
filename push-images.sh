242c6667f6bed7147c44fddffe6fc0e276e8a1f1
Jeff Chen <jeff.chen@circleci.com>
2023-08-02 12:12:07 -0700

    save commit

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/go:1.19
-docker push cimg/go:1.19.11
-docker push cimg/go:1.19-node
-docker push cimg/go:1.19.11-node
-docker push cimg/go:1.19-browsers
-docker push cimg/go:1.19.11-browsers
+set -eo pipefail
