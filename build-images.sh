cfe7522ea4cdd16fb49d6e1ed9ea435ef6a5d8df
Jeff Chen <jeff.chen@circleci.com>
2023-08-07 12:58:07 -0700

    remove commit: updated dockerfiles

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/Dockerfile -t 483285841698.dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com/go:1.19.11 -t 483285841698.dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com/go:1.19 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/node/Dockerfile -t 483285841698.dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com/go:1.19.11-node -t 483285841698.dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com/go:1.19-node --push .
-docker buildx build --platform=linux/amd64 --file 1.19/browsers/Dockerfile -t 483285841698.dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com/go:1.19.11-browsers -t 483285841698.dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com/go:1.19-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/Dockerfile -t cimg/go:1.19.12 -t cimg/go:1.19 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/node/Dockerfile -t cimg/go:1.19.12-node -t cimg/go:1.19-node --push .
+docker buildx build --platform=linux/amd64 --file 1.19/browsers/Dockerfile -t cimg/go:1.19.12-browsers -t cimg/go:1.19-browsers --push .
