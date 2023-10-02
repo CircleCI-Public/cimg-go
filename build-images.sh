aa19bed5929ea69b5e846349642753cd19322150
Jeff Chen <jeff.chen@circleci.com>
2023-10-02 11:20:24 -0700

    test

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,4 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/Dockerfile -t cimg/go:1.19.13 -t cimg/go:1.19 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/node/Dockerfile -t cimg/go:1.19.13-node -t cimg/go:1.19-node --push .
-docker buildx build --platform=linux/amd64 --file 1.19/browsers/Dockerfile -t cimg/go:1.19.13-browsers -t cimg/go:1.19-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.19/Dockerfile -t ferriswh33l/node-dev:1.19.13 -t ferriswh33l/go:1.19 --push .
